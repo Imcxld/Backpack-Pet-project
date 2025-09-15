dcfb4cbec132287658594f3d51891beec84d54bd
Imcxld <Teoamiyune.work@gmail.com>
2025-09-15 15:19:53 +0300

    Add project files.

---
+++ Backpack.App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Backpack.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
